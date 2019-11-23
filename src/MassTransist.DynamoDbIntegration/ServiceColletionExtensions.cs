1221b4937277fc31b4fa9df9d3548c3473e69ffd
Fernando Seguim <fseguim@acesso.com>
2019-11-23 17:46:45 -0300

    implement service collection extension methods

---
+++ src/MassTransist.DynamoDbIntegration/ServiceColletionExtensions.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using GreenPipes.Internals.Extensions;
+using MassTransist.DynamoDbIntegration.DatabaseContext;
+using MassTransist.DynamoDbIntegration.Saga;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MassTransist.DynamoDbIntegration
+{
+    [ExcludeFromCodeCoverage]
+    public static class ServiceColletionExtensions
+    {
+        /// <summary>
+        /// Configure DynamoDb saga repository 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configure"></param>
+        public static void AddDynamoDbEventStore<TSaga>(this IServiceCollection services, Action<DynamoDbEventStoreOptions> configure = null) where TSaga : class, IEventSourcedSaga
+        {
+            var options = new DynamoDbEventStoreOptions();
+            configure?.Invoke(options);
+
+            var name = typeof(TSaga).Name;
+            if(options.StoreName is null) { options.StoreName = $"MassTransist.DynamoDbIntegration.{name}"; }
+            
+            services.AddSingleton(options);
+            var provider = services.BuildServiceProvider();
+
+            var knownEventTypes = provider.GetService<KnownEventTypes>();
+
+            if(knownEventTypes is null) throw new InvalidOperationException("Known events types should be registered before register saga repository. Consider use RegisterKnownEventsTypes(params Type[] knownEvetTypes)");
+            var client = new AmazonDynamoDBClient(options.Credentials, options.Region);
+
+            //TODO: change by services.AddAWSService<IAmazonDynamoDB>(options);
+            services.AddSingleton<IAmazonDynamoDB>(client);
+            
+            services.AddSingleton<IDynamoDbEventStoreDatabaseContext, DynamoDbEventStoreDatabaseContext>();
+            services.AddAsyncInitializer<DynamoDbEventStoreDatabaseContextInitializer>();
+
+            var repository = new DynamoDbSagaRepository<TSaga>(new DynamoDBContext(client), options, knownEventTypes);
+            services.AddSingleton<DynamoDbSagaRepository<TSaga>>(repository);
+        }
+
+        /// <summary>
+        /// Register known events types that will be used during saga orquestration
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="knownEvetTypes"></param>
+        public static void RegisterKnownEventsTypes(this IServiceCollection services, params Type[] knownEvetTypes)
+        {
+            var knownTypes = new KnownEventTypes();
+            knownTypes.RegisterTypes(knownEvetTypes);
+            services.AddSingleton<KnownEventTypes>(knownTypes);
+        }
+    }
+}
